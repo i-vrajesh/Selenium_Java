395ff2e8f4d549637ec761107ad9ded37af1d987
vrajesh <vatlurirajesh@gmail.com>
2024-12-14 23:47:30 +0530

    First Commit

--- src/test/java/Pages/LoginTest.java
+++ src/test/java/Pages/LoginTest.java
@@ -1,4 +1,19 @@
 package Pages;
 
-public class Login {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class LoginTest {
+    WebDriver driver;
+    public void loginTest(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.close();
+    }
 }
