e046caaad96899b224e0d23265b621ed80d32a06
vrajesh <vatlurirajesh@gmail.com>
2025-03-22 21:38:17 +0530

    Authentication Popup

---
+++ src/test/java/Selenium/AuthenticationPopup.java
@@ -0,0 +1,31 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class AuthenticationPopup {
+    public static void main(String[] args) throws AWTException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://admin:password@the-internet.herokuapp.com/basic_auth");
+//        String message = driver.findElement(By.tagName("p")).getText();
+//        System.out.println("Message :"+message);
+        Robot robot = new Robot();
+        robot.delay(2000);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_D);
+        robot.keyRelease(KeyEvent.VK_D);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.keyPress(KeyEvent.VK_P);
+        robot.keyRelease(KeyEvent.VK_P);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyRelease(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        driver.quit();
+    }
+}
